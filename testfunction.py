2adca537825a02aabaead00c0b8dcfa21c015a9d
abhijitpradhanrp-dev <abhijitpradhan.rp@gmail.com>
2026-03-09 13:05:40 +0530

    testcase

---
+++ testfunction.py
@@ -0,0 +1,10 @@
+def add (a,b):
+    return a + b
+
+def subtract (a,b):
+    return a - b    
+
+c= add(10,20)
+print("The sum of a and b is:", c)
+d= subtract(30,20)
+print("The difference of a and b is:", d)
